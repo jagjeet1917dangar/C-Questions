9fa24cd845a7fd8aff39ed062310d51be60a2f4b
Jagjeet Dangar <jagjeet.dangar.cg@gmail.com>
2025-01-18 15:52:31 +0530

    38 questions

--- 38th Question.cpp
+++ 38th Question.cpp
@@ -13,3 +13,20 @@ int main() {
     cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
     return 0;
 }
+
+### 2nd approach ###
+
+#include <iostream>
+using namespace std;
+float convert(float celsius) {
+    int hello=(celsius * 9 / 5) + 32;
+    return hello;
+}
+int main() {
+    float celsius;
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;
+    float fahrenheit = convert(celsius);
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+    return 0;
+}
